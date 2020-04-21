dbb8402db3c076fa000c12cb8b9acfce8850f362
seizze <cwcwc3@gmail.com>
2020-04-21 23:13:22 +0900

    [#8] feat: 배지 색상 선언

---
+++ iOS/SideDish/SideDish/Utils/UIColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtension.swift
+//  SideDish
+//
+//  Created by Chaewan Park on 2020/04/21.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let badgeDarkGray = UIColor(named: "Badge Dark Gray")!
+    static let badgePurple = UIColor(named: "Badge Purple")!
+    static let badgeYellow = UIColor(named: "Badge Yellow")!
+    static let pointMint = UIColor(named: "Point Mint")!
+}
