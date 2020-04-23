ba21a40e1a2990254b69c227ceef80a8f79166d1
최장호 <hmchoi651@naver.com>
2020-04-23 20:56:44 +0900

    [#10] feat: Menu Component 구성 및 hover시 List Active를 판별하는 IsShown State 정의

---
+++ FE/side-dish/src/components/menuTab/MenuItem.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import MenuList from "./MenuList.jsx";
+import { makeStyles } from "@material-ui/core/styles";
+
+const MenuItem = ({ title, list }) => {
+  const classes = useStyles();
+  const [isShown, setIsShown] = useState(false);
+
+  return <div></div>;
+};
+
+export default MenuItem;
