c39327810a60fe2d0e39ca097a6a163b0ff8f041
최장호 <hmchoi651@naver.com>
2020-04-23 21:05:03 +0900

    [#10] docs : 테스트 라인 삭제

--- FE/side-dish/src/App.js
+++ FE/side-dish/src/App.js
@@ -4,15 +4,12 @@ import Header from "./components/header/Header";
 import Menu from "./components/menuTab/Menu";
 import "./css/reset.css";
 
-import Testing from "./components/test.jsx";
-
 const SideDish = () => {
   return (
     <>
       <Nav />
       <Header />
       <Menu />
-      <Testing />
     </>
   );
 };
