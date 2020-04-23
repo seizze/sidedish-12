6ba880756f5ab11a2bbb17e12a1cd40275c78bd7
최장호 <hmchoi651@naver.com>
2020-04-23 20:33:43 +0900

    [#10] feat : List가 있는 NavItem 기본 구성 데이터

---
+++ FE/side-dish/src/components/nav/constant/navConstants.js
@@ -0,0 +1,19 @@
+const appItems = {
+  title: "배민찬 앱 다운로드",
+  name: "app",
+  options: ["안드로이드 앱 다운로드", "애플 앱 다운로드"],
+};
+
+const myPageItems = {
+  title: "페이지",
+  name: "page",
+  options: ["주문현황", "1:1 문의", "교환/반품", "등급별해택/쿠폰함", "포인트"],
+};
+
+const customerCenterItems = {
+  title: "고객센터",
+  name: "center",
+  options: ["공지사항", "자주 묻는 질문", "새벽배송안내", "정기배송안내"],
+};
+
+export { appItems, myPageItems, customerCenterItems };
