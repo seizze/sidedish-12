a0b425dc9c2eb1d67307c7cda595d9e140ed6dbc
최장호 <hmchoi651@naver.com>
2020-04-23 20:40:57 +0900

    [#10] feat: Nav Component State값 정의

---
+++ FE/side-dish/src/components/nav/Nav.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import NavItem from "./NavItem";
+import NavSelectItem from "./NavSelectItem";
+import {
+  appItems,
+  myPageItems,
+  customerCenterItems,
+} from "./constant/navConstants.js";
+
+const Nav = () => {
+  const classes = useStyles();
+
+  const [appListData] = useState(appItems);
+
+  const [myPageListData] = useState(myPageItems);
+
+  const [customerCenterListData] = useState(customerCenterItems);
+
+  const [appActive, setAppActive] = useState(false);
+  const [myPageActive, setMyPageActive] = useState(false);
+  const [centerActive, setCenterActive] = useState(false);
+
+  return <div></div>;
+};
+
+export default Nav;
