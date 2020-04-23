31d5f20509c79d3bdc81b80d0ca0697cafaff6f2
최장호 <hmchoi651@naver.com>
2020-04-23 20:31:30 +0900

    [#10] docs , feat : Style 관련 라이브러리 Install , 필요없는 코드 삭제

--- FE/side-dish/src/index.js
+++ FE/side-dish/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SideDish from "./SideDish";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <SideDish />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
