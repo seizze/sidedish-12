42ee33ab4a37c4d19bbb70f2ff0350094c094845
최장호 <hmchoi651@naver.com>
2020-04-23 20:44:15 +0900

    [#10] feat : Nav Component Render 구현

--- FE/side-dish/src/components/nav/Nav.jsx
+++ FE/side-dish/src/components/nav/Nav.jsx
@@ -29,7 +29,43 @@ const Nav = () => {
     }
   };
 
-  return <div></div>;
+  return (
+    <div className={classes.root}>
+      <div>
+        <NavSelectItem
+          ItemInfo={appListData}
+          onActive={() => {
+            onActiveHandler(appActive, setAppActive);
+          }}
+          active={appActive}
+        />
+      </div>
+
+      <div className={classes.navRightStyles}>
+        <NavItem title="로그인" />
+        <NavItem title="회원가입" />
+
+        <NavSelectItem
+          ItemInfo={myPageListData}
+          onActive={() => {
+            onActiveHandler(myPageActive, setMyPageActive);
+          }}
+          active={myPageActive}
+        />
+        <NavSelectItem
+          ItemInfo={customerCenterListData}
+          onActive={() => {
+            onActiveHandler(centerActive, setCenterActive);
+          }}
+          active={centerActive}
+        />
+
+        <NavItem title="새벽배송 지역검색" />
+        <NavItem title="이벤트 게시판" />
+        <NavItem title="장바구니" />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
