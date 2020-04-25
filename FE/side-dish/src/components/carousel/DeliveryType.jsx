91872c2249b3f105cbbf9375dc97514a6159211f
최장호 <hmchoi651@naver.com>
2020-04-25 16:58:36 +0900

    [#20] feat: Mouse hover시에 item delivery 정보를 보여주는 Component

---
+++ FE/side-dish/src/components/carousel/DeliveryType.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const DeliveryType = ({ deliveryDatas }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.deliveryTypeStyles}>
+      {deliveryDatas.map((el) => {
+        return <div>{el}</div>;
+      })}
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  deliveryTypeStyles: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    top: "0%",
+    position: "absolute",
+    width: "300px",
+    height: "300px",
+    borderRadius: "50%",
+    backgroundColor: "black",
+    opacity: "0.7",
+    color: "#fff",
+    "& div": {
+      fontSize: "26px",
+      fontWeight: "bold",
+      borderBottom: "3px solid #fff",
+    },
+  },
+});
+
+export default DeliveryType;
