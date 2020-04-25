0a2272cdaa81ceef92904100339dd9ed7ed0ab90
koo bohyeon <goosee115@gmail.com>
2020-04-26 01:08:24 +0900

    [#30]refactor: repository 수정
    query문을 수정하면서 repository의 메소드 수정하고 controller에 적용함
    mapper를 만들어서 중복 코드 제거

--- BE/src/main/java/com/team12/sidedish/controller/ProductController.java
+++ BE/src/main/java/com/team12/sidedish/controller/ProductController.java
@@ -27,34 +27,31 @@ public class ProductController {
     @GetMapping("/banchans")
     public ResponseDto getBanchansByCategory(@RequestParam(name = "category") String categoryName) {
         Category category = categoryRepository.findByCategoryName(categoryName);
+        List<Dish> dishes = dishRepository.findByCategoryId(category.getId());
 
-        List<Dish> dishes = dishRepository.findDishByCategoryId(category.getId());
-
-//      최상위 카테고리인 경우
-        if (dishes.size() > 0) {
+//      하위 카테고리가 없는 경우
+        if (!dishes.isEmpty()) {
             CategoryDto categoryDto = new CategoryDto(category);
             List<DishDto> dishDtos = dishes.stream().map(DishDto::new).collect(Collectors.toList());
             categoryDto.setItems(dishDtos);
             return ResponseDto.OK(categoryDto);
-        } else {
-//            하위 카테고리들이 있는 경우
-            List<Category> categories = categoryRepository.findByCategoryParent(category.getId());
-            List<CategoryDto> categoryDtos = categories.stream().map(c -> {
-                CategoryDto childCategoryDtos = new CategoryDto(c);
-                List<DishDto> childCategoryDishes = dishRepository.findDishByCategoryId(c.getId())
-                        .stream()
-                        .map(DishDto::new)
-                        .collect(Collectors.toList());
-                childCategoryDtos.setItems(childCategoryDishes);
-                return childCategoryDtos;
-            }).collect(Collectors.toList());
-
-            return ResponseDto.OK(categoryDtos);
         }
+
+//      하위 카테고리가 있는 경우
+        List<Category> categories = categoryRepository.findSubCategoryByParentId(category.getId());
+        List<CategoryDto> categoryDtos = categories.stream().map(c -> {
+            CategoryDto subCategoryDtos = new CategoryDto(c);
+            List<Dish> subDishes = dishRepository.findByCategoryId(c.getId());
+            List<DishDto> subDishDtos = subDishes.stream().map(DishDto::new).collect(Collectors.toList());
+            subCategoryDtos.setItems(subDishDtos);
+            return subCategoryDtos;
+        }).collect(Collectors.toList());
+
+        return ResponseDto.OK(categoryDtos);
     }
 
     @GetMapping("/banchans/{banchanId}")
-    public ResponseDto getBanchan(@PathVariable String banchanId) {
+    public ResponseDto<DetailDishDto> getBanchan(@PathVariable String banchanId) {
         Dish dish = dishRepository.findById(banchanId);
         DetailDishDto detailDishDto = new DetailDishDto(dish);
 
--- BE/src/main/java/com/team12/sidedish/repository/DishRepository.java
+++ BE/src/main/java/com/team12/sidedish/repository/DishRepository.java
@@ -1,6 +1,7 @@
 package com.team12.sidedish.repository;
 
 import com.team12.sidedish.domain.Dish;
+import com.team12.sidedish.util.DishMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Repository
 public class DishRepository {
@@ -28,70 +26,18 @@ public class DishRepository {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public List<Long> getDishIds(Long categoryId) {
-        String sql = "SELECT * FROM dish_category where category = ?";
-        List<Long> dishIds = new ArrayList<>();
-
-        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql, categoryId);
-        for (Map row : rows) {
-            Long id = (Long) row.get("dish");
-            dishIds.add(id);
+    public List<Dish> findByCategoryId(Long categoryId) {
+        String sql = "select  dish.id ,dish.image, dish.alt, dish.delivery_type, dish.title, dish.description, dish.badge, dish.top_image,dish.thumb_images, dish.point, dish.delivery_info, dish.delivery_fee, dish.s_price, dish.n_price, dish.detail_section " +
+                "from category INNER JOIN dish_category INNER JOIN dish where category.id= "+ categoryId + " AND dish_category.category = category.id AND dish.id = dish";
+        try {
+            return jdbcTemplate.query(sql, new DishMapper());
+        } catch (IndexOutOfBoundsException e) {
+            return null;
         }
-
-        return dishIds;
     }
 
     public Dish findById(String dishId) {
         String sql = "SELECT * FROM dish where id = ?";
-
-        return jdbcTemplate.queryForObject(sql, new Object[]{dishId}, (rs, rowNum) -> {
-            Dish dish = new Dish();
-            dish.setId(rs.getLong("id"));
-            dish.setImage(rs.getString("image"));
-            dish.setAlt(rs.getString("alt"));
-            dish.setDeliveryType(rs.getString("delivery_type"));
-            dish.setTitle(rs.getString("title"));
-            dish.setDescription(rs.getString("description"));
-            dish.setBadge(rs.getString("badge"));
-            dish.setTopImage(rs.getString("top_image"));
-            dish.setThumbImages(rs.getString("thumb_images"));
-            dish.setPoint(rs.getString("point"));
-            dish.setDeliveryInfo(rs.getString("delivery_info"));
-            dish.setDeliveryFee(rs.getString("delivery_fee"));
-            dish.setN_price(rs.getString("n_price"));
-            dish.setS_price(rs.getString("s_price"));
-            dish.setDetailSection(rs.getString("detail_section"));
-            return dish;
-        });
+        return jdbcTemplate.queryForObject(sql, new Object[]{dishId}, new DishMapper());
     }
-
-    //    category 하위 dish
-    public List<Dish> findDishByCategoryId(Long categoryId) {
-        List<Long> dishIds = getDishIds(categoryId);
-        return dishIds.stream().map(id -> {
-            String findDishSql = "SELECT * FROM dish where id = ?";
-            List<Map<String, Object>> dishRows = jdbcTemplate.queryForList(findDishSql, id);
-            Dish dish = new Dish();
-            for (Map row : dishRows) {
-                dish.setId((Long) row.get("id"));
-                dish.setImage((String) row.get("image"));
-                dish.setAlt((String) row.get("alt"));
-                dish.setDeliveryType((String) row.get("delivery_type"));
-                dish.setTitle((String) row.get("title"));
-                dish.setDescription((String) row.get("description"));
-                dish.setBadge((String) row.get("badge"));
-                dish.setTopImage((String) row.get("top_image"));
-                dish.setThumbImages((String) row.get("thumb_images"));
-                dish.setPoint((String) row.get("point"));
-                dish.setDeliveryInfo((String) row.get("delivery_info"));
-                dish.setDeliveryFee((String) row.get("delivery_fee"));
-                dish.setN_price((String) row.get("n_price"));
-                dish.setS_price((String) row.get("s_price"));
-                dish.setDetailSection((String) row.get("detail_section"));
-            }
-            return dish;
-        }).collect(Collectors.toList());
-    }
-
-
 }
