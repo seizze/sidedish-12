3006adff3c7554399b630f9489e350a20dfe66c4
koo bohyeon <goosee115@gmail.com>
2020-04-27 22:55:58 +0900

    [#45]feature: 환경에 따라 github oAuth 관련 값을 다르게 넣어준다.
    properties 파일을 따로 만들고 변수를 넣어줌
    환경에 따라 다른 값을 읽어 넣어 줄 수 있도록 함

--- BE/src/main/java/com/team12/sidedish/controller/LoginController.java
+++ BE/src/main/java/com/team12/sidedish/controller/LoginController.java
@@ -2,8 +2,8 @@ package com.team12.sidedish.controller;
 
 import com.team12.sidedish.domain.LoginToken;
 import com.team12.sidedish.domain.User;
-import com.team12.sidedish.dto.ResponseDto;
 import com.team12.sidedish.service.LoginService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +13,26 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
+
+    @Value("${github_redirect_url}")
+    private String redirect_url;
+
+    @Value("${github_client_id}")
+    private String client_id;
+
+    @Value("${github_client_secret}")
+    private String client_secret;
 
     private static final LoginService loginService = new LoginService();
 
     @GetMapping("/githubLogin")
     public RedirectView githubLogin() {
         RedirectView redirectView = new RedirectView();
-        String redirectUrl = loginService.getRedirectUrl();
+        String redirectUrl = loginService.getRedirectUrl(client_id, redirect_url);
 
         redirectView.setUrl(redirectUrl);
         return redirectView;
@@ -30,7 +40,7 @@ public class LoginController {
 
     @GetMapping("/login")
     public RedirectView login(@PathParam("code") String code, HttpServletResponse httpServletResponse) {
-        LoginToken response = loginService.getAccessToken(code);
+        LoginToken response = loginService.getAccessToken(code, client_id, client_secret, redirect_url);
         User responseUer = loginService.getUserInfo(response.getTokenType(), response.getAccessToken());
 
         String jws = loginService.generateJwtToken(responseUer.getUserId());
