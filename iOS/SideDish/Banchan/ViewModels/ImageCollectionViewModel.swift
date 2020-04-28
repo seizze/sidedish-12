cbc6e6960153d22228ff806b5d18671fda46bd73
seizze <cwcwc3@gmail.com>
2020-04-29 05:27:20 +0900

    [#60] feat: Image Collection 뷰모델 추가

---
+++ iOS/SideDish/Banchan/ViewModels/ImageCollectionViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  ImageCollectionViewModel.swift
+//  Banchan
+//
+//  Created by Chaewan Park on 2020/04/29.
+//  Copyright © 2020 Chaewan Park. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewModel: ViewModelBinding {
+    typealias Key = [Int: UIImage?]?
+    
+    private var images: Key = nil {
+        didSet { changeHandler(images) }
+    }
+    
+    private var changeHandler: (Key) -> Void
+    
+    init(with images: Key = nil, handler: @escaping (Key) -> Void = { _ in }) {
+        self.changeHandler = handler
+        self.images = images
+        changeHandler(images)
+    }
+    
+    func update(images: Key) {
+        self.images = images
+    }
+    
+    func updateNotify(handler: @escaping (Key) -> Void) {
+        self.changeHandler = handler
+    }
+}
