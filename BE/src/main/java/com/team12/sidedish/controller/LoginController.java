108a67a56cbee2f3d22bf00b5c0a1bc752d813c8
koo bohyeon <goosee115@gmail.com>
2020-04-28 12:53:47 +0900

    [#23]fix:oauth 관련 설정 수정함
    redirect url 값을 넣어줌

--- BE/src/main/java/com/team12/sidedish/controller/LoginController.java
+++ BE/src/main/java/com/team12/sidedish/controller/LoginController.java
@@ -2,8 +2,8 @@ package com.team12.sidedish.controller;
 
 import com.team12.sidedish.domain.LoginToken;
 import com.team12.sidedish.domain.User;
-import com.team12.sidedish.dto.ResponseDto;
 import com.team12.sidedish.service.LoginService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,24 +13,38 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
+
+    @Value("base_url")
+    private String baseUrl;
+
+    @Value("github_client_id")
+    private String client_id;
+
+    @Value("github_client_secret")
+    private String client_secret;
+
+    @Value("github_redirect_url")
+    private String redirectUrl;
 
     private static final LoginService loginService = new LoginService();
 
     @GetMapping("/githubLogin")
     public RedirectView githubLogin() {
-        RedirectView redirectView = new RedirectView();
-        String redirectUrl = loginService.getRedirectUrl();
 
-        redirectView.setUrl(redirectUrl);
+        RedirectView redirectView = new RedirectView();
+        String url = loginService.getRedirectUrl(client_id, redirectUrl);
+
+        redirectView.setUrl(url);
         return redirectView;
     }
 
     @GetMapping("/login")
     public RedirectView login(@PathParam("code") String code, HttpServletResponse httpServletResponse) {
-        LoginToken response = loginService.getAccessToken(code);
+        LoginToken response = loginService.getAccessToken(code, client_id, client_secret, redirectUrl);
         User responseUer = loginService.getUserInfo(response.getTokenType(), response.getAccessToken());
 
         String jws = loginService.generateJwtToken(responseUer.getUserId());
@@ -42,7 +56,7 @@ public class LoginController {
         httpServletResponse.addCookie(cookie);
         RedirectView redirectView = new RedirectView();
 
-        redirectView.setUrl("/");
+        redirectView.setUrl(baseUrl);
 
         return redirectView;
     }
