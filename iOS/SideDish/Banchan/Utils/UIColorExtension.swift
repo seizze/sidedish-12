2ba05ff0fbd97ed4051932712e4001e2a60462b2
seizze <cwcwc3@gmail.com>
2020-05-02 22:31:55 +0900

    [#60] refactor: 필요 없는 코드 삭제

--- iOS/SideDish/Banchan/Utils/UIColorExtension.swift
+++ iOS/SideDish/Banchan/Utils/UIColorExtension.swift
@@ -12,16 +12,3 @@ extension UIColor {
     static let pointMint = UIColor(named: "Point Mint")!
     static let badgePurple = UIColor(named: "Badge Purple")!
 }
-
-extension UIColor {
-    convenience init(red: Int, green: Int, blue: Int) {
-        self.init(red: CGFloat(red) / 255.0,
-                  green: CGFloat(green) / 255.0,
-                  blue: CGFloat(blue) / 255.0,
-                  alpha: 1.0)
-    }
-    
-    convenience init(inHex hexValue: Int) {
-        self.init(red:(hexValue >> 16) & 0xff, green:(hexValue >> 8) & 0xff, blue:hexValue & 0xff)
-    }
-}
